7477b91c09c20c2140d63670dac1a39497f019a8
yiranchen111 <145508882+yiranchen111@users.noreply.github.com>
2024-03-03 11:36:02 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var toggleButton = document.getElementById("toggle-high-contrast");
+toggleButton.addEventListener("click", function() {
+   // var body = document.getElementsByTagName("body")[0];
+    //body.classList.toggle("high-contrast");
+    document.body.classList.toggle("high-contrast")
+});
